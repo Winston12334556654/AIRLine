f6d0c6306167abaaac7b94814ff81f6ac8ecd88e
Winston12334556654 <marannoe1322001@gmailcom>
2024-04-18 23:20:52 +0630

    Passenger Feld integration

--- flights/models.py
+++ flights/models.py
@@ -17,3 +17,8 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length= 64)
+    flights = models.ManyToManyField(Flight, blank= True,  )
